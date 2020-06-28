35ce999aed231215a7627f595e872500e533e39c
Marshmellowon <leo503801@gmail.com>
2020-06-28 15:15:07 +0900

    csv 하는중

---
+++ save.py
@@ -0,0 +1,9 @@
+import csv
+
+
+# TODO: 2.15 1:41까지 들음
+
+def save_to_file(jobs):
+    file = open("jobs.csv", mode="w")
+    print(file)
+    return
--- so.py
+++ so.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 
+print("scrappint url: ", URL)
+
 
 def get_last_page():
     result = requests.get(URL)
@@ -17,27 +19,29 @@ def extract_job(html):
     company, location = html.find("h3").find_all("span", recursive=False)
     company = company.get_text(strip=True).strip()
     location = location.get_text(strip=True).strip("-").strip("\r").strip("\n")
+    job_id = html["data-jobid"]
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'apply-link': f"https://stackoverflow.com/jobs/{job_id}"
+    }
 
-    return {'title': title, 'company': company, 'location': location}
-
-    # TODO: return이 출력이 안됨 해결할것 2.12 3:16까지 봄
-
-
-# TODO: 아 리턴 안됨
 
 def extract_jobs(last_page):
-    jobs = []
+    returnjob = []
     for page in range(last_page):
+        print(f"Scrapping SO:  {page}")
         result = requests.get(f"{URL}&pg={page + 1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
             job = extract_job(result)
-            jobs.append(job)
-    return jobs
+            returnjob.append(job)
+    return returnjob
 
 
 def get_jobs():
     last_page = get_last_page()
-    jobs = extract_jobs(last_page)
-    return jobs
+    jobs2 = extract_jobs(last_page)
+    return jobs2
